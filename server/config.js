c11d8f0c5a4640aa63ea0dea7f7865a761d18c65
Camilo Giraldo <camilogiraldo91@gmail.com>
2017-01-16 21:05:08 -0500

    create server for instaSatellizer app

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: process.env.db || 'localhost',
+  clientSecret: process.env.clientSecret || 'c098d21b933a48a5834d93f9506fcb21',
+  tokenSecret: process.env.tokenSecret || 'pick a hard to guess string'
+}
